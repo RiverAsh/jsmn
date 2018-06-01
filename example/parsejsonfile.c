1d29407f306747de6b792fd133cbd903882a5118
kangjae <kang_jae@hotmail.com>
2018-06-01 10:24:20 +0900

    e

--- example/parsejsonfile.c
+++ example/parsejsonfile.c
@@ -28,7 +28,7 @@ int main() {
 	// strncpy(filename, "data.json", 20);
 
 	char * JSON_STRING = ReadJsonFile(filename);
-	//	printf("%s\n",JSON_STRING);
+	printf("%s\n",JSON_STRING);
 	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t) / sizeof(t[0]));
 
 	if (r < 0) {
